3d43fa3f80154f38cc5dc124934c0714efa2cbec
ShaoyuTu <kevin.yu.tu@gmail.com>
2023-04-30 02:40:56 -0700

    Included basic main.js code and connected to html

---
+++ main.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+    // Smooth scroll effect for anchor links
+    $('a[href^="#"]').on('click', function(event) {
+      var target = $(this.getAttribute('href'));
+      if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+          scrollTop: target.offset().top - 70 // adjust the offset to fit your layout
+        }, 1000);
+      }
+    });
+  
+    // Sticky header effect
+    $(window).on("scroll", function() {
+      var header = $(".header");
+      var scroll = $(window).scrollTop();
+  
+      if (scroll >= 100) {
+        header.addClass("sticky");
+      } else {
+        header.removeClass("sticky");
+      }
+    });
+  
+    // Responsive menu toggle
+    $(".menu-toggle").on("click", function() {
+      $("nav").toggleClass("open");
+    });
+  
+    // Close responsive menu when clicking outside of it
+    $(document).on("click", function(event) {
+      if (!$(event.target).closest(".header").length) {
+        $("nav").removeClass("open");
+      }
+    });
+  
+    // Filter projects by category
+    $(".filter-nav li").on("click", function() {
+      var category = $(this).attr("data-category");
+      if (category == "all") {
+        $(".project-item").show();
+      } else {
+        $(".project-item").not("." + category).hide();
+        $(".project-item." + category).show();
+      }
+      $(this).addClass("active").siblings().removeClass("active");
+    });
+  });
+  
